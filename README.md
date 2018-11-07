542c245379131aa18776374693da8b50923baa80
devzons <devzons@gmail.com>
2018-11-07 11:29:41 -0800

    basic setup of Yelppi campgrounds app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Yelppi
+## Yelppi
+
+* Add Landing Page
+* Add Campgrounds Page that lists all compgrounds
+
+Each Compground has:
+* Name
+* Image
---
+++ v1/app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get('/', function(req, res){
+  res.render("landing");
+});
+
+app.get('/campgrounds', function(req, res){
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144594f5c27ba7e8b4_340.jpg"},
+    {name: "Copper River", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144594f5c27ba7e8b4_340.jpg"},
+    {name: "Indian Creek", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f1c47aa5edb0b9_340.jpg"}
+  ];
+
+  res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+
+app.listen(4000, function(req, res){
+  console.log("The Yeppi server has started!!!");
+});
